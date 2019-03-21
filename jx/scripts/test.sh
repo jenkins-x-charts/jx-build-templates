baaf140f5950736deb6d62d08386907f3f3e4dfd
Gareth Evans <gareth@bryncynfelin.co.uk>
2019-03-21 16:43:05 +0000

    debug: displaying jq version

--- jx/scripts/test.sh
+++ jx/scripts/test.sh
@@ -2,6 +2,8 @@
 
 set -euo pipefail
 
+jq --version
+
 JENKINS_TAG=$(yq r jx-build-templates/values.yaml jenkinsTag)
 BUILDERS_TAG=$(yq r jx-build-templates/values.yaml builderTag)
 IMAGES=$(cat jx-build-templates/templates/*.yaml | grep image | grep -v allure | sed 's/- image: //' | sed 's/image: //' | sed "s/{{ .Values.jenkinsTag }}/$JENKINS_TAG/" | sed "s/{{ .Values.builderTag }}/$BUILDERS_TAG/" )
